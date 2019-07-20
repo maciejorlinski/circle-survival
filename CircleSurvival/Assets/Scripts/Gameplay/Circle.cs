6867250bec095b162dd3e303ea82b0c60a2c5065
Maciej Orlinski <maciejorlinski1@gmail.com>
2019-07-20 14:01:54 +0200

    Added green and black circle prefabs

---
+++ CircleSurvival/Assets/Scripts/Gameplay/Circle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Circle : MonoBehaviour, IPointerClickHandler {
+    private Timer timer;
+    private Action onClick;
+
+    private void Awake() {
+        timer = GetComponent<Timer>();
+    }
+
+    public void Initialize(float lifeTime, Action onClick, Action onTimeout) {
+        timer.Set(lifeTime, onTimeout);
+        this.onClick = onClick;
+    }
+
+    public void OnPointerClick(PointerEventData eventData) {
+        timer.Stop();
+        onClick?.Invoke();
+    }
+}
---
+++ CircleSurvival/Assets/Scripts/UI/Timer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    #region Inspector
+    public Image image;
+    #endregion
+
+    private float goal;
+    private IEnumerator countdown;
+    private float time;
+
+    public void Set(float goal, Action callback) {
+        this.goal = goal;
+        countdown = Countdown(callback);
+        StartCoroutine(countdown);
+    }
+
+    public void Stop() {
+        if (countdown != null) {
+            StopCoroutine(countdown);
+            countdown = null;
+        }
+    }
+
+    private IEnumerator Countdown(Action callback) {
+        time = 0;
+        while (time < goal) {
+            time += Time.deltaTime;
+            if (image != null)
+                image.fillAmount = time / goal;
+            yield return null;
+        }
+        callback?.Invoke();
+        countdown = null;
+    }
+}
