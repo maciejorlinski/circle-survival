f16a0afedac0d62d4f5642a921262caa040fab4f
Maciej Orlinski <maciejorlinski1@gmail.com>
2019-07-20 14:01:26 +0200

    Added scriptable object with gameplay balance data

---
+++ CircleSurvival/Assets/Scripts/Data/GameplayBalanceData.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "BalanceData", menuName = "Data/Balance Data")]
+public class GameplayBalanceData : ScriptableObject {
+    public float startingFrequency;
+    public float frequencyIncreaseOverTime;
+    public float blackCircleFrequency = 0.1f;
+    public Vector2 startingLifeTimeRange = new Vector2(2, 4);
+    public Vector2 lifeTimeRangeModifierOverTime;
+
+    [ContextMenu("Check game time")]
+    public void GameTimeCheck() {
+        float timeUntilZeroLifeTime = -startingLifeTimeRange.x / lifeTimeRangeModifierOverTime.x;
+        float timeUntilAlwaysSpawn = (1 - startingFrequency) / frequencyIncreaseOverTime;
+        float timeUntilRangeInvalid = 
+            (startingLifeTimeRange.y - startingLifeTimeRange.x) / 
+            (lifeTimeRangeModifierOverTime.y - lifeTimeRangeModifierOverTime.x);
+        Debug.LogFormat("Zero life time in {0:0.###}s, always spawn in {1:0.###}s. Range invalid in {2:0.###}s.", timeUntilZeroLifeTime, timeUntilAlwaysSpawn, timeUntilRangeInvalid);
+    }
+}
